55e3fb5edb824cd572e641fe36640a8fc74116f1
qiheng.yang <xy162162a@163.com>
2018-08-08 16:56:08 +0800

    第一次上传

---
+++ testmodule/src/main/java/com/tom/testmodule/TestMethods.java
@@ -0,0 +1,31 @@
+package com.tom.testmodule;
+
+/**
+ * Created by TomLeisen on 2018/8/8 下午4:50
+ * Email: xy162162a@163.com
+ * Description:
+ */
+public class TestMethods {
+
+    public static String test;
+    public static String test2;
+
+    public TestMethods() {
+    }
+
+    public static String getTest() {
+        return test;
+    }
+
+    public static void setTest(String test) {
+        TestMethods.test = test;
+    }
+
+    public static String getTest2() {
+        return test2;
+    }
+
+    public static void setTest2(String test2) {
+        TestMethods.test2 = test2;
+    }
+}
